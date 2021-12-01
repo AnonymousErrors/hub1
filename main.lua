1f694f241c4fbb87e54e9a49cad1275ab0651269
Hunt <zuner.hunter@gmail.com>
2021-12-01 07:20:18 +0200

    newww

---
+++ main.lua
@@ -0,0 +1,16 @@
+local pid = game.PlaceId
+if pid == 6951762614 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/ufosim.lua"))()
+elseif pid == 7065731541 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/speedmansim.lua"))()
+elseif pid == 7025847767 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/destroyedkidsim.lua"))()
+elseif pid == 7363174769 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/deliverymansim.lua"))()
+elseif pid == 6766156863 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/strongmansim.lua"))()
+elseif pid == 7336128600 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/PetLegends.lua"))()
+elseif pid == 6769764667 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/FactorySimulator.lua"))()
+end
